3bbd896adae13fb50174de70cb16aa48593017a6
ilyasarrafii <zainularrafii@gmail.com>
2024-05-15 20:48:46 +0700

    product resource and fixing upload image

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -6,12 +6,19 @@ use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
 use Filament\Forms;
+use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Group;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
+use Filament\Support\RawJs;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Storage;
 
 class ProductResource extends Resource
 {
@@ -23,55 +30,88 @@ class ProductResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\Select::make('category_id')
-                    ->relationship('category', 'name'),
-                Forms\Components\FileUpload::make('image')
-                    ->image(),
+                Group::make([
+                    FileUpload::make('image')
+                        ->image()
+                        ->disk('public')
+                        ->maxSize(1024)
+                        ->directory('images/products')
+                        ->imageCropAspectRatio('1:1'),
+                ])->columns(2)
+                    ->columnSpan([
+                        'lg' => 2
+                    ]),
                 Forms\Components\TextInput::make('name')
                     ->required()
-                    ->maxLength(255),
+                    ->live(500)
+                    ->maxLength(255)
+                    ->afterStateUpdated(fn (Set $set, $state) => $set('sku', str($state . '-' . str()->random())->slug())),
                 Forms\Components\TextInput::make('sku')
                     ->label('SKU')
                     ->required()
-                    ->maxLength(255),
-                Forms\Components\Textarea::make('description')
-                    ->required()
+                    ->maxLength(255)
+                    ->suffixAction(function (Forms\Set $set, Forms\Get $get) {
+                        return Forms\Components\Actions\Action::make('generateSku')
+                            ->icon('heroicon-o-arrow-path')
+                            ->hidden(!$get('name'))
+                            ->action(fn () => $set('sku', str($get('name') . '-' . str()->random())->slug()));
+                    }),
+                Forms\Components\Select::make('category_id')
+                    ->relationship('category', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+                Forms\Components\MarkdownEditor::make('description')
+                    ->maxLength(65535)
                     ->columnSpanFull(),
-                Forms\Components\TextInput::make('stock_quantity')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('price')
-                    ->required()
-                    ->numeric()
-                    ->prefix('$'),
-                Forms\Components\TextInput::make('cost_price')
-                    ->required()
-                    ->numeric(),
+                Group::make([
+                    Forms\Components\TextInput::make('stock_quantity')
+                        ->required()
+                        ->numeric(),
+                    Forms\Components\TextInput::make('cost_price')
+                        ->mask(RawJs::make('$money($input)'))
+                        ->required()
+                        ->prefix('Rp'),
+                    Forms\Components\TextInput::make('price')
+                        ->mask(RawJs::make('$money($input)'))
+                        ->required()
+                        ->prefix('Rp')
+                        ->live(500),
+
+                ])->columns(3)->columnSpanFull(),
+
             ]);
+        dd($form);
     }
 
     public static function table(Table $table): Table
     {
         return $table
+            ->defaultSort('created_at', 'desc')
             ->columns([
-                Tables\Columns\TextColumn::make('category.name')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\ImageColumn::make('image'),
-                Tables\Columns\TextColumn::make('name')
-                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image')->circular()->disk('public'),
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('category.name'),
                 Tables\Columns\TextColumn::make('sku')
                     ->label('SKU')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('stock_quantity')
+                    ->label('Qty')
                     ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('price')
-                    ->money()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cost_price')
                     ->numeric()
+                    ->prefix('Rp ')
                     ->sortable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->numeric()
+                    ->prefix('Rp ')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->limit(500)
+                    ->color('gray')
+                    ->placeholder('-')
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -89,7 +129,16 @@ class ProductResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\DeleteBulkAction::make()
+                        ->after(function (Product $record) {
+                            if ($record->image) {
+                                Storage::disk('public')->delete($record->image);
+                            }
+
+                            if ($record->image) {
+                                foreach ($record->image as $ph) Storage::disk('public')->delete($ph);
+                            }
+                        }),
                 ]),
             ]);
     }
